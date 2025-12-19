18e308bfd9839363f74af78843d7d77ffc9303f7
Sanjay <attellisanjay29@gmail.com>
2025-12-19 17:44:56 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* ADD ALL CUSTOM ROUTES and  ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
